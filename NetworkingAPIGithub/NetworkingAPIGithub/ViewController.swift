1e9a12ba5b7d6e29b32953b64124f94edbeeb564
Raeid Saqur <raeidsaqur@gmail.com>
2017-04-25 15:48:42 -0400

    Preliminary code

---
+++ NetworkingAPIGithub/NetworkingAPIGithub/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NetworkingAPIGithub
+//
+//  Created by Raeid Saqur on 2017-04-25.
+//  Copyright © 2017 Raeid Saqur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
